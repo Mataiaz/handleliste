4827146eb8d607c10f77694395d6285292fd403c
Mataiaz <mathias344@live.no>
2022-10-22 02:01:49 +0200

    Added home page, create_list page, history page and settings page. Added Navigation routes to each page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:handleliste/pages/home.dart';
+import 'package:handleliste/pages/create_list.dart';
+import 'package:handleliste/pages/history.dart';
+import 'package:handleliste/pages/settings.dart';
 
-void main() {
-}
+//Here we set the navigation routes for each page
+void main() => runApp(MaterialApp(
+  home: Home(),
+  routes: {
+    '/home': (context) => Home(),
+    '/create_list': (context) => CreateList(),
+    '/history': (context) => History(),
+    '/settings': (context) => Settings(),
+  },
+));
---
+++ lib/pages/create_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreateList extends StatefulWidget {
+  const CreateList({ Key? key }) : super(key: key);
+
+  @override
+  State<CreateList> createState() => _CreateListState();
+}
+
+class _CreateListState extends State<CreateList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
